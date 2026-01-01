7e0e7eb1e69ced563446cbb3b2a62bea2d4d9e1f
rohit-2604 <rohitneogi26042002@gmail.com>
2026-01-01 20:11:42 +0530

    Update package.json main entry and modify MongoDB connection to use config.uri

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-
+import { config } from "../../constants.js";
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(config.uri);
     console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
