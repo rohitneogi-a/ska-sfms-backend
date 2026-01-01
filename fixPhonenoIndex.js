08bc6fc3ba2a8d00349cb4f434105188c2f0e1c3
rohit-2604 <rohitneogi26042002@gmail.com>
2026-01-01 20:18:26 +0530

    Fix import path for User model in fixPhonenoIndex.js

--- fixPhonenoIndex.js
+++ fixPhonenoIndex.js
@@ -1,5 +1,5 @@
 // import mongoose from "mongoose";
-// import User from "./src/models/user.model.js";
+// import User from "../models/user.model.js";
 
 // const MONGO_URI = ""; // <-- update this
 
